c99e37d87a6a0dca92c60092afc6ea0ba8f7cc89
Justin Singleton <38257985+JustinSingleton303@users.noreply.github.com>
2021-02-17 13:37:08 -0700

    Dice animation working
    now we just need to add the message for who won.

--- dicee.js
+++ dicee.js
@@ -1,8 +1,15 @@
 /* dicee.js javaScript file that dicee.html
    uses to make the dice game run */
 
+var dicePics = ["images/dice1.png", "images/dice2.png", "images/dice3.png",
+                "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+
 var r1wins = false;
 var r2wins = false;
+// get rolls for comparison
+var p1roll = dieRoll();
+var p2roll = dieRoll();
+compareRolls(p1roll, p2roll);
 
 function dieRoll(){
   var roll = Math.floor(Math.random() * 6) + 1;
@@ -11,9 +18,38 @@ function dieRoll(){
 
 function compareRolls(r1, r2){
   if(r1 < r2){
-    r1wins = true;
-  }
-  else if(r1 > r2){
     r2wins = true;
   }
+  else if(r1 > r2){
+    r1wins = true;
+  }
+  else{
+    r1wins = false;
+    r2wins = false;
+  }
 }
+
+function gameResult(){
+  if(r1wins){
+    return "Player one wins!";
+  }
+  else if(r2wins){
+    return "Player two wins!";
+  }
+  else{
+    return "It's a tie!";
+  }
+}
+
+function getImage(a){
+  var dImg = dicePics[a - 1];
+  return dImg;
+}
+
+var testImage = getImage(3);
+var winner = gameResult();
+//document.querySlector(".winner")
+document.querySelector(".img1").setAttribute("src", getImage(p1roll));
+document.querySelector(".img2").setAttribute("src", getImage(p2roll));
+console.log(testImage);
+console.log(p1roll + " " + p2roll);
